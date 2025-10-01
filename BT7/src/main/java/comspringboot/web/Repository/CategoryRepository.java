109e2cbb6acba2bd1ac1824d45f444293c766ea2
Nguyễn Thuận Phú <162897309+Akashi169@users.noreply.github.com>
2025-10-01 15:15:35 +0700

    Delete BT7 directory

--- BT7/src/main/java/comspringboot/web/Repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package comspringboot.web.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import comspringboot.web.Entity.CategoryEntity;
-
-public interface CategoryRepository extends JpaRepository<CategoryEntity,Long>{
-
-}
--- BT7/src/main/java/comspringboot/web/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package comspringboot.web.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import comspringboot.web.Entity.ProductEntity;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-
-}
